799c507247bb48f5b2e7e68485ee0bee528b09d6
bharat <bhk2705@gmail.com>
2021-04-20 16:54:13 +0530

    changes by tayyab

--- src/components/Wrapper/githubLogoText.tsx
+++ src/components/Wrapper/githubLogoText.tsx
@@ -6,6 +6,7 @@ import logo from '../../assets/github_logo.png';
 import * as styles from '../../styles';
 
 export const GithubLogoText = () => {
+  console.log('as')
   return (
     <div className={styles.cardLogo}>
       <img alt={'github logo'} className={styles.cardLogoGithub} src={logo} />
